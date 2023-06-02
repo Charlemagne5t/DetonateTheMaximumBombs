d65770f8e349793c61b12f66de5b13bf5a6a6a19
alek <wait82985@gmail.com>
2023-06-02 12:10:04 +0600

    initial commit. 3 test cases

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Solution {
+    public int maximumDetonation(int[][] bombs) {
+        //TODO
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,38 @@
+import org.example.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void maximumDetonationTest1() {
+        int[][] bombs = {
+                {2, 1, 3},
+                {6, 1, 4}
+        };
+        int output = 2;
+        Assert.assertEquals(output, new Solution().maximumDetonation(bombs));
+    }
+
+    @Test
+    public void maximumDetonationTest2() {
+        int[][] bombs = {
+                {1, 1, 5},
+                {10, 10, 5}
+        };
+        int output = 1;
+        Assert.assertEquals(output, new Solution().maximumDetonation(bombs));
+    }
+
+    @Test
+    public void maximumDetonationTest3() {
+        int[][] bombs = {
+                {1, 2, 3},
+                {2, 3, 1},
+                {3, 4, 2},
+                {4, 5, 3},
+                {5, 6, 4}
+        };
+        int output = 5;
+        Assert.assertEquals(output, new Solution().maximumDetonation(bombs));
+    }
+}
